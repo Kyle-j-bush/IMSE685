b8a01dbbb9a09519c15b41f6a0ff8a2a7adf2124
Kyle-J-Bush <kyle@bushinc.com>
2026-01-12 21:41:07 -0600

    "added actual code"

---
+++ introduction-code/03-introduction.py
@@ -0,0 +1,22 @@
+# my github delted this when I did 12 and 15 so I made a tldr
+x: str = "Hello World"
+print(x + x)
+print(x + "I am so sad")
+print(x.upper())
+
+# Print formating: dont use .format use f stirng
+
+print(f"YO! {x}")  # tbh this is all you need. Use AI for any other formating
+
+# lists are muttable and ordered also allow dublicaitons. Lists are sick
+
+my_list: list = ["string", 100, 2.3]
+print(len(my_list))
+print(my_list[0])
+print(my_list[0:])
+another_list = [1, 2, "2"]
+print(my_list + another_list)
+# You can use .append just look this up there are like 100 different list functions you can use
+
+# pop is acutually cool because it is o(1) and can do stuff with stacks can use indexing
+print(my_list.pop())
